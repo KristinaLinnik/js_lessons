bb1dfb77e9c32bf043a1c811b8adc45d0cdc8ac7
Kristina Frenkel <func.kris@gmail.com>
2020-02-06 22:47:31 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+let leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+let person =  {
+    name: 'John',
+    age: '25',
+    isMarried: false
+};
+console.log(person.name);
+console.log(person['name']);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+console.log(arr[2]);
+console.log(arr[0]);
+console.log(arr[1]);
+
+
